c7dc1f01e72131022e77b67f0ece7930e3a2e3f8
TSASOFFICIAL <156132823+TSASOFFICIAL@users.noreply.github.com>
2024-10-20 17:38:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,15 +10,18 @@ function getCookie(cname) {
 		if (c.indexOf(name) == 0) {
 			return c.substring(name.length, c.length);
 		}
-	}
+	};
 	return "";
-}
+};
+
 if (getCookie("username") == "") {
 	window.location.href = "https://tsasofficial.github.io/login";
-}
+};
 
 var a_element = document.getElementById("logout_a");
 
-if (getCookie("usingmicrosoftaccount") == "true") {
-	a_element.setAttribute("href", "microsoftlogout");
-}
+   if (a_element) { // Check if a_element is not null
+       if (getCookie("usingmicrosoftaccount") == "true") {
+           a_element.setAttribute("href", "microsoftlogout");
+       }
+   };
